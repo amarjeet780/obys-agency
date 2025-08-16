bd6eb25a8638fd1969c95ce25ad1003f9d87ac71
amarjeet720 <amarjeetkumar17dc@gmail.com>
2025-08-16 17:17:35 +0530

    update2

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+function locomotiveAnimation(){
+        gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
 function loadindgAnimation(){
     var tl = gsap.timeline() 
 tl.from(".line h1" , {
@@ -9,8 +41,8 @@ tl.from(".line h1" , {
 tl.from(".line h2" , {
     opacity:0,
     onStart: function(){       // jab ye hoha to function start hoga
-        var h5Timer = document.querySelector("#line-part1 h5");
-var grow=0 ;
+    var h5Timer = document.querySelector("#line-part1 h5");
+    var grow=0 ;
 setInterval(function(){
     if(grow<100){
         grow++;
@@ -24,8 +56,8 @@ setInterval(function(){
     }
 })
 tl.to(".line h2", {
-    animationName:"anime",
-    opacity:1 ,
+    animationName:"loader-anime",
+    opacity:1,
 })
 tl.to("#loader", {
     opacity:0 ,
@@ -62,3 +94,4 @@ Shery.makeMagnet("#nav-part2 h4");
 
 loadindgAnimation();
 csrsAnmation();
+locomotiveAnimation();
