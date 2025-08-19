648af73982119908bc8b726121556badcf7a0f58
amarjeet720 <amarjeetkumar17dc@gmail.com>
2025-08-19 12:00:49 +0530

    complete without resp.

--- app.js
+++ app.js
@@ -79,6 +79,9 @@ tl.from("#hero1 h1, #hero2 h2 , #hero3 h1", {
     y:120,              
     stagger:0.2 ,
 })
+tl.from("#hero1 , #page2" , {
+    opacity:0,
+}, "-=1.2")
 }
 
 function csrsAnmation(){
@@ -88,10 +91,98 @@ function csrsAnmation(){
         top:dets.y,
     })
 })
-
-Shery.makeMagnet("#nav-part2 h4");
+ Shery.makeMagnet("#nav-part2 h4");
 }
+document.querySelector("#flag").addEventListener("mousemove", function(dets){
+    gsap.to("#flag" , {
+        x:dets.x ,
+        y:dets.y ,
+    })
+})
+document.querySelector("#hero2").addEventListener("mouseenter", function(){
+    gsap.to("#flag" , {
+        opacity:1, 
+    })
+})
+document.querySelector("#hero2").addEventListener("mouseleave", function(){
+    gsap.to("#flag" , {
+        opacity:0, 
+    })
+})
+
+var videoContainer = document.querySelector("#video-container");
+var video = document.querySelector("#video-container video")
+videoContainer.addEventListener("mouseenter" , function(){
+    videoContainer.addEventListener("mousemove" , function(dets){
+        gsap.to("#crsr" , {
+            opacity:0,
+        })
+        gsap.to("#cursor" , {
+        left:dets.x - 400,
+        y:dets.y - 200, 
+    });
+    });
+});
+videoContainer.addEventListener("mouseleave" , function(){
+        gsap.to("#crsr" , {
+            opacity:1 ,
+        });
+        gsap.to("#cursor" , {
+            top:"-15%",
+            left:"70%",
+        })
+    });
+
+    var flag=0;
+    videoContainer.addEventListener("click" , function(){
+        if(flag==0){
+        video.play()
+        video.style.opacity = 1;
+
+        document.querySelector("#cursor").innerHTML = `<i class="ri-pause-mini-line"></i>`
+        gsap.to("#cursor" , {
+            scale:0.5,
+        })
+        flag=1;
+    }
+    else{
+         video.pause()
+        video.style.opacity = 0;
+
+        document.querySelector("#cursor").innerHTML = `<i class="ri-play-fill"></i>`
+        gsap.to("#cursor" , {
+            scale:1,
+        })
+        flag=0;
+    }
+    });
+function sheryAnimation(){
+    Shery.imageEffect(".img-div",{
+        style:5 ,
+        config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.7501875468867217,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7272749669846593},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+        gooey:true 
+    })
+}
+
+var textAnime = document.querySelector("#footer h1");
+textAnime.addEventListener("mouseover" , function(){
+    gsap.from("#footer h1" , {
+        opacity:0 ,
+        duration: 1,
+        delay: 0.5,
+        onStart:function(){
+            $('#footer h1').textillate({ in: { effect: 'fadeIn' } });
+        }
+    }) ;
+});
+textAnime.addEventListener("mouseleave" , function(){
+   gsap.to("#footer h1" , {
+    opacity:1 ,
+    duration:0.3 
+   });
+});
 
 loadindgAnimation();
 csrsAnmation();
 locomotiveAnimation();
+sheryAnimation();
